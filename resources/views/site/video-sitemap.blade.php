d927fb372fc1469ea3e69d4c14719aa64e82faad
rozalia-dev <info@emeraldrozalia.ie>
2026-09-09 13:28:22 +0100

    Make video sitemap resilient to legacy timestamps

--- resources/views/site/video-sitemap.blade.php
+++ resources/views/site/video-sitemap.blade.php
@@ -1,6 +1,7 @@
 {!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($videos as $video)
-    <url><loc>{{ route('videos.watch',$video->uuid) }}</loc><lastmod>{{ $video->updated_at->toAtomString() }}</lastmod></url>
+    @php($lastmod = $video->updated_at ?: $video->created_at)
+    <url><loc>{{ route('videos.watch',$video->uuid) }}</loc>@if($lastmod)<lastmod>{{ \Illuminate\Support\Carbon::parse($lastmod)->toAtomString() }}</lastmod>@endif</url>
 @endforeach
 </urlset>
