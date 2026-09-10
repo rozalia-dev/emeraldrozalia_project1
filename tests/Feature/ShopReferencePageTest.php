1a4b119afb94e7396d3e470e516560fccb1a080c
rozalia-dev <info@emeraldrozalia.ie>
2026-09-10 14:36:45 +0100

    Correct Shop reference contract assertion

--- tests/Feature/ShopReferencePageTest.php
+++ tests/Feature/ShopReferencePageTest.php
@@ -16,7 +16,7 @@ class ShopReferencePageTest extends TestCase
         $this->get('/shop')
             ->assertOk()
             ->assertSee([
-                'SHOP ALL HATS &amp; CAPS',
+                'SHOP ALL HATS & CAPS',
                 'Irish made. Premium quality. Made in Limerick.',
                 'IRISH MADE',
                 'WORLDWIDE DELIVERY',
