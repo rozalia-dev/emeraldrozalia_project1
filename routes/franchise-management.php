8dc32f31ba63cccc5887dd7108903198e6bb1737
rozalia-dev <info@emeraldrozalia.ie>
2026-09-10 19:14:03 +0100

    Route territories through dedicated dashboard

--- routes/franchise-management.php
+++ routes/franchise-management.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\Admin\FranchiseManagementController;
+use App\Http\Controllers\Admin\FranchiseTerritoryController;
 use Illuminate\Support\Facades\Route;
 
 $sectionPattern = implode('|', FranchiseManagementController::SECTIONS);
 
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () use ($sectionPattern): void {
+    Route::get('/resource/franchise-territories', [FranchiseTerritoryController::class, 'index'])
+        ->name('admin.franchise.territories');
+
+    Route::prefix('franchise-management/franchise-territories')->name('admin.franchise.territories.')->group(function (): void {
+        Route::get('/export', [FranchiseTerritoryController::class, 'export'])->name('export');
+        Route::post('/', [FranchiseTerritoryController::class, 'store'])->name('store');
+        Route::patch('/{territory}', [FranchiseTerritoryController::class, 'update'])->name('update');
+        Route::delete('/{territory}', [FranchiseTerritoryController::class, 'destroy'])->name('destroy');
+    });
+
     Route::get('/resource/{section}', [FranchiseManagementController::class, 'show'])
         ->where('section', $sectionPattern)
         ->name('admin.franchise.page');
