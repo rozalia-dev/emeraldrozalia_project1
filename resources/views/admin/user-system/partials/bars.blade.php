8d8e2c63b178df857c4ae73855c4655271f6afe0
rozalia-dev <info@emeraldrozalia.ie>
2026-09-10 17:55:21 +0100

    Fix User System analytics bar rendering

--- resources/views/admin/user-system/partials/bars.blade.php
+++ resources/views/admin/user-system/partials/bars.blade.php
@@ -1,2 +1,20 @@
-@php $max=max(1,collect($items ?? [])->max('value') ?: 1); @endphp
-<div class="us-bars">@forelse($items ?? [] as $item)<div><span>{{\Illuminate\Support\Str::limit((string)$item['label'],26)}}</span><i><b style="width:{{round(($item['value']/$max)*100)}}%"></b></i><em>{{$item['value']}}</em></div>@empty<p class="us-empty">No data yet.</p>@endforelse</div>
+@php
+    $barItems = collect($items ?? [])->map(function ($item) {
+        return [
+            'label' => (string) data_get($item, 'label', ''),
+            'value' => (float) data_get($item, 'value', data_get($item, 'aggregate', 0)),
+        ];
+    });
+    $max = max(1, (float) ($barItems->max('value') ?: 1));
+@endphp
+<div class="us-bars">
+    @forelse($barItems as $item)
+        <div>
+            <span>{{ \Illuminate\Support\Str::limit($item['label'], 26) }}</span>
+            <i><b style="width:{{ round(($item['value'] / $max) * 100) }}%"></b></i>
+            <em>{{ $item['value'] == (int) $item['value'] ? (int) $item['value'] : $item['value'] }}</em>
+        </div>
+    @empty
+        <p class="us-empty">No data yet.</p>
+    @endforelse
+</div>
