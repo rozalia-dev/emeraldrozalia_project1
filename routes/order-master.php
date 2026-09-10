91348901dc344ef42e29b956e74b132e231a7f7d
rozalia-dev <info@emeraldrozalia.ie>
2026-09-10 15:32:59 +0100

    Point Order Master routes to overview controller

--- routes/order-master.php
+++ routes/order-master.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Admin\OrderMasterController;
+use App\Http\Controllers\Admin\OrderMasterOverviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware(['web', 'auth', 'admin'])->name('admin.')->group(function (): void {
-    Route::get('/order-master', [OrderMasterController::class, 'overview'])->name('order-master.overview');
-    Route::post('/order-master', [OrderMasterController::class, 'store'])->name('order-master.store');
-    Route::get('/order-master/export', [OrderMasterController::class, 'export'])->name('order-master.export');
-    Route::post('/order-master/import', [OrderMasterController::class, 'import'])->name('order-master.import');
+    Route::get('/order-master', [OrderMasterOverviewController::class, 'index'])->name('order-master.overview');
+    Route::post('/order-master', [OrderMasterOverviewController::class, 'store'])->name('order-master.store');
+    Route::get('/order-master/export', [OrderMasterOverviewController::class, 'export'])->name('order-master.export');
+    Route::post('/order-master/import', [OrderMasterOverviewController::class, 'import'])->name('order-master.import');
 });
