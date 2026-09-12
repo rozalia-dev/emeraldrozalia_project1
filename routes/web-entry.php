50a5bb175d3042599a404bc66a22919943df89ba
rozalia-dev <info@emeraldrozalia.ie>
2026-09-12 19:08:44 +0600

    Build pixel-matched banner and slider management

--- routes/web-entry.php
+++ routes/web-entry.php
@@ -5,4 +5,5 @@ require __DIR__.'/communication-center.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/sales-reports.php';
 require __DIR__.'/reports.php';
+require __DIR__.'/banners.php';
 require __DIR__.'/web.php';
