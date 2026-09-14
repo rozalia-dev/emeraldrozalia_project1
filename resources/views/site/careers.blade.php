f377048377b9594b392dae4c85c7792a2432dc07
rozalia-dev <info@emeraldrozalia.ie>
2026-09-15 01:14:30 +0600

    Render responsive careers UUID logo

--- resources/views/site/careers.blade.php
+++ resources/views/site/careers.blade.php
@@ -12,7 +12,7 @@
         <div class="career-hero-visual" aria-label="Emerald Rozalia careers in Limerick">
             <div class="career-visual-glow"></div>
             <div class="career-visual-brand">
-                @if($careersLogo)<img src="{{ $careersLogo['url'] }}" alt="{{ $careersLogo['alt'] }}">@else<span class="public-media-missing">Emerald Rozalia Limited</span>@endif
+                @if($careersLogo)<img src="{{ $careersLogo['url'] }}" @if($careersLogo['srcset']) srcset="{{ $careersLogo['srcset'] }}" sizes="{{ $careersLogo['sizes'] }}" @endif width="{{ $careersLogo['width'] ?: '' }}" height="{{ $careersLogo['height'] ?: '' }}" alt="{{ $careersLogo['alt'] }}">@else<span class="public-media-missing">Emerald Rozalia Limited</span>@endif
                 <span>CAREERS · LIMERICK, IRELAND</span>
             </div>
             <div class="career-visual-copy">
