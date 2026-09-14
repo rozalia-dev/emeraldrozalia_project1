fef68769ab56950edcafda22dafabadf5a829669
rozalia-dev <info@emeraldrozalia.ie>
2026-09-15 04:16:19 +0600

    Enable universal cPanel export formats

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ApplySeoRedirects;
 use App\Http\Middleware\AttachRequestCorrelation;
 use App\Http\Middleware\EnsureAdmin;
+use App\Http\Middleware\EnsureAdminExportFormats;
 use App\Http\Middleware\EnsurePermission;
 use App\Http\Middleware\EnsureFranchisePermission;
 use App\Http\Middleware\EnsureCommunicationPermission;
@@ -26,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: env('TRUSTED_PROXIES', 'REMOTE_ADDR'));
         $middleware->appendToGroup('web', ResolveTenantContext::class);
         $middleware->appendToGroup('web', AttachRequestCorrelation::class);
+        $middleware->appendToGroup('web', EnsureAdminExportFormats::class);
         $middleware->alias([
             'admin' => EnsureAdmin::class,
             'permission' => EnsurePermission::class,
