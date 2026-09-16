9cd9fb56f9b75ac938537bdf30690d63540467af
rozalia-dev <info@emeraldrozalia.ie>
2026-09-17 01:38:00 +0600

    Add public franchise requirements document route

--- routes/chat-24-7.php
+++ routes/chat-24-7.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PublicChatController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/franchise/requirements', 'site.franchise-requirements')->name('franchise.requirements');
+
 Route::prefix('chat-24-7')->middleware(['web', 'throttle:60,1'])->name('chat24.')->group(function (): void {
     Route::post('/start', [PublicChatController::class, 'start'])->name('start');
     Route::post('/{conversation}/messages', [PublicChatController::class, 'send'])->whereUuid('conversation')->name('send');
