fd91b61b74ed684e936dfbcbdde32a74eb4ecf83
rozalia-dev <info@emeraldrozalia.ie>
2026-09-17 00:55:21 +0600

    Apply web session middleware to Chat 24/7 routes

--- routes/chat-24-7.php
+++ routes/chat-24-7.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PublicChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('chat-24-7')->middleware('throttle:60,1')->name('chat24.')->group(function (): void {
+Route::prefix('chat-24-7')->middleware(['web', 'throttle:60,1'])->name('chat24.')->group(function (): void {
     Route::post('/start', [PublicChatController::class, 'start'])->name('start');
     Route::post('/{conversation}/messages', [PublicChatController::class, 'send'])->whereUuid('conversation')->name('send');
     Route::get('/{conversation}/messages', [PublicChatController::class, 'messages'])->whereUuid('conversation')->name('messages');
