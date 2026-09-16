673017912ad5ec6e0233836c5b92c0bbab3dd280
rozalia-dev <info@emeraldrozalia.ie>
2026-09-17 05:24:12 +0600

    Add taxonomy country and club routes

--- routes/categories.php
+++ routes/categories.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CatalogTaxonomyController;
 use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,17 @@ Route::prefix('admin/resource/categories')
         Route::post('/reorder', [CategoryController::class, 'reorder'])->name('reorder');
         Route::post('/import', [CategoryController::class, 'import'])->name('import');
         Route::get('/export', [CategoryController::class, 'export'])->name('export');
+
+        Route::get('/taxonomy', [CatalogTaxonomyController::class, 'index'])->name('taxonomy');
+        Route::post('/taxonomy/build', [CatalogTaxonomyController::class, 'build'])->name('taxonomy.build');
+        Route::get('/countries', [CatalogTaxonomyController::class, 'countries'])->name('countries');
+        Route::post('/countries', [CatalogTaxonomyController::class, 'storeCountry'])->name('countries.store');
+        Route::patch('/countries/{country}', [CatalogTaxonomyController::class, 'updateCountry'])->name('countries.update');
+        Route::get('/clubs', [CatalogTaxonomyController::class, 'clubs'])->name('clubs');
+        Route::post('/clubs', [CatalogTaxonomyController::class, 'storeClub'])->name('clubs.store');
+        Route::patch('/clubs/{club}', [CatalogTaxonomyController::class, 'updateClub'])->name('clubs.update');
+        Route::delete('/clubs/{club}', [CatalogTaxonomyController::class, 'destroyClub'])->name('clubs.destroy');
+
         Route::post('/{category}/visibility', [CategoryController::class, 'toggleVisibility'])->name('visibility');
         Route::get('/{category}/audit', [CategoryController::class, 'audit'])->name('audit');
         Route::patch('/{category}', [CategoryController::class, 'update'])->name('update');
