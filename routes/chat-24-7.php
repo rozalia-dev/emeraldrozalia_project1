07a2a4fb067e09a1931889a0680dbeb445d8889b
rozalia-dev <info@emeraldrozalia.ie>
2026-09-17 01:41:52 +0600

    Apply web middleware to franchise requirements page

--- routes/chat-24-7.php
+++ routes/chat-24-7.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\PublicChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/franchise/requirements', 'site.franchise-requirements')->name('franchise.requirements');
+Route::view('/franchise/requirements', 'site.franchise-requirements')
+    ->middleware('web')
+    ->name('franchise.requirements');
 
 Route::prefix('chat-24-7')->middleware(['web', 'throttle:60,1'])->name('chat24.')->group(function (): void {
     Route::post('/start', [PublicChatController::class, 'start'])->name('start');
