188f32fee1985c9232d0a2cff440029bb27c698a
rozalia-dev <info@emeraldrozalia.ie>
2026-09-16 21:45:32 +0600

    Register discount bulk lifecycle endpoint

--- routes/admin-table-actions.php
+++ routes/admin-table-actions.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DiscountBulkActionController;
 use App\Http\Controllers\Admin\GenericResourceBulkController;
 use App\Http\Controllers\Admin\MediaBulkActionController;
 use App\Http\Controllers\Admin\PageBulkActionController;
@@ -19,6 +20,8 @@ Route::middleware(['auth', 'admin'])->group(function (): void {
     Route::prefix('admin/table-actions')
         ->name('admin.table-actions.')
         ->group(function (): void {
+            Route::post('/discounts', DiscountBulkActionController::class)
+                ->name('discounts');
             Route::post('/product-media', [MediaBulkActionController::class, 'productMedia'])
                 ->name('product-media');
             Route::post('/site-media', [MediaBulkActionController::class, 'siteMedia'])
