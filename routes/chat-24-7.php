4d7fd0b6e33ed590ca22e8f3689ecdb5d4da874b
rozalia-dev <info@emeraldrozalia.ie>
2026-09-17 02:44:53 +0600

    Expose appointment availability to public scheduler

--- routes/chat-24-7.php
+++ routes/chat-24-7.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppointmentAvailabilityController;
 use App\Http\Controllers\PublicChatController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,7 @@ Route::view('/franchise/requirements', 'site.franchise-requirements')
     ->name('franchise.requirements');
 
 Route::prefix('chat-24-7')->middleware(['web', 'throttle:60,1'])->name('chat24.')->group(function (): void {
+    Route::get('/appointments/availability', AppointmentAvailabilityController::class)->name('appointments.availability');
     Route::post('/start', [PublicChatController::class, 'start'])->name('start');
     Route::post('/{conversation}/messages', [PublicChatController::class, 'send'])->whereUuid('conversation')->name('send');
     Route::get('/{conversation}/messages', [PublicChatController::class, 'messages'])->whereUuid('conversation')->name('messages');
