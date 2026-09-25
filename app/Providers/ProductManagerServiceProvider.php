a865fe34c46b62852e96cfd097fa23e3de16f845
rozalia-dev <info@emeraldrozalia.ie>
2026-09-26 03:50:16 +0600

    Add Product Manager print and download routes

--- app/Providers/ProductManagerServiceProvider.php
+++ app/Providers/ProductManagerServiceProvider.php
@@ -16,6 +16,8 @@ class ProductManagerServiceProvider extends ServiceProvider
                 ->name('admin.product-manager.')
                 ->group(function (): void {
                     Route::get('/', [ProductManagerController::class, 'index'])->name('index');
+                    Route::get('/print', [ProductManagerController::class, 'print'])->name('print');
+                    Route::get('/download', [ProductManagerController::class, 'download'])->name('download');
                     Route::post('/bulk-publish', [ProductManagerController::class, 'bulkPublish'])->name('bulk-publish');
                     Route::post('/{product}/publish', [ProductManagerController::class, 'publish'])->whereNumber('product')->name('publish');
                     Route::delete('/bulk', [ProductManagerController::class, 'bulkDestroy'])->name('bulk-destroy');
